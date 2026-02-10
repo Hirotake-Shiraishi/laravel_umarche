8b6352455ad0a0cb48820228e247e1ef24068f44
hirotake-shiraishi <srishrtk710@gmail.com>
2026-02-10 18:12:19 +0900

    delete ソフトデリート 処理側

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,12 +1,12 @@
 @props(['status' => 'info'])
 
 @php
-    if ($status === 'info') {
+    if (session('status') === 'info') {
         $bgColor = 'bg-blue-300';
     }
-    // if ($status === 'error') {
-    //     $bgColor = 'bg-red-500';
-    // }
+    if (session('status') === 'alert') {
+        $bgColor = 'bg-red-500';
+    }
 @endphp
 
 @if (session('message'))
