dcac1604766ab74ca4d2a7acf6f5311d5373f7bc
hirotake-shiraishi <srishrtk710@gmail.com>
2026-02-12 10:29:23 +0900

    Shop 外部キー制約

---
+++ database/seeders/ShopSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ShopSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('shops')->insert([
+            [
+                'owner_id' => 1,
+                'name' => 'ここに店名が入ります',
+                'information' => 'ここにお店の情報が入ります。ここにお店の情報が入ります。ここにお店の情報が入ります。',
+                'filename' => '',
+                'is_selling' => true
+            ],
+            [
+                'owner_id' => 2,
+                'name' => 'ここに店名が入ります',
+                'information' => 'ここにお店の情報が入ります。ここにお店の情報が入ります。ここにお店の情報が入ります。',
+                'filename' => '',
+                'is_selling' => true
+            ],
+        ]);
+    }
+}
