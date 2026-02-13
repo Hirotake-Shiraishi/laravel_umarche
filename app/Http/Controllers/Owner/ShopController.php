1dd0048349c43694da890227d1957a0389325513
hirotake-shiraishi <srishrtk710@gmail.com>
2026-02-13 19:17:46 +0900

    Shop コントローラミドルウェア

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -12,6 +12,26 @@ class ShopController extends Controller
     public function __construct()
     {
         $this->middleware('auth:owners');
+
+        // クロージャを使ったコントローラミドルウェア
+        $this->middleware(function ($request, $next) {
+
+            // dd($request->route()->parameter('shop'));
+
+            $id = $request->route()->parameter('shop'); //URLパラメータ shopのid取得
+
+            if (!is_null($id)) { //null判定
+
+                $shopsOwnerId = Shop::findOrFail($id)->owner_id;
+
+                $ownerId = Auth::id();
+
+                if ($shopsOwnerId !== $ownerId) { // 同じでなかったら
+                    abort(404); // 404画面表示
+                }
+            }
+            return $next($request);
+        });
     }
 
     public function index()
@@ -24,13 +44,9 @@ class ShopController extends Controller
         return view('owner.shops.index', compact('shops'));
     }
 
-    public function edit($id)
-    {
-
+    public function edit($id) {
+        echo 'edit';
     }
 
-    public function update(Request $request, $id)
-    {
-
-    }
+    public function update(Request $request, $id) {}
 }
