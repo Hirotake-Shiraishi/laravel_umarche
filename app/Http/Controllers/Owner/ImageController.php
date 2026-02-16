283e0cd31aab13a92b204af7e0a7f20792df9518
hirotake-shiraishi <srishrtk710@gmail.com>
2026-02-17 05:23:41 +0900

    Image Index

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -4,17 +4,41 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Image;
+use Illuminate\Support\Facades\Auth;
 
 class ImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        // クロージャを使ったコントローラミドルウェア
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('images'); //URLパラメータ imagesのid取得
+
+            if (!is_null($id)) {
+
+                $imagesOwnerId = Image::findOrFail($id)->owner_id;
+
+                // セッションのログインIDと相違があったら
+                if ($imagesOwnerId !== Auth::id()) {
+                    abort(404); // 404画面表示
+                }
+            }
+            return $next($request);
+        });
+    }
+
     public function index()
     {
-        //
+        // owner_id ・セッションのログインIDが一致するレコードだけを取得する
+        $images = Image::where('owner_id', Auth::id())
+        ->orderBy('updated_at', 'desc') //降順に並び替え（更新頻度が高い順）
+        ->paginate(20); //paginateの時は、get不要
+
+        return view('owner.images.index', compact('images'));
     }
 
     /**
--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Owner;
 
 class Image extends Model
 {
@@ -13,4 +14,11 @@ class Image extends Model
         'owner_id',
         'filename',
     ];
+
+    // ownerメソッドの作成
+    // メソッド名は、リレーションするモデル名と対応させる。
+    public function owner()
+    {
+        return $this->belongsTo(Owner::class);
+    }
 }
