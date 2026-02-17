f5b3fc08bdb7efe96d5c31c1c12b327b7101530a
hirotake-shiraishi <srishrtk710@gmail.com>
2026-02-17 15:15:18 +0900

    Image destroy

--- resources/views/owner/images/edit.blade.php
+++ resources/views/owner/images/edit.blade.php
@@ -31,7 +31,7 @@
                                     </div>
                                 </div>
                             </div>
-                            {{-- ボタン --}}
+                            {{-- 更新ボタン/戻るボタン --}}
                             <div class="p-2 w-full flex justify-around mt-4">
                                 <button type="button" onclick="location.href='{{ route('owner.images.index') }}'"
                                     class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
@@ -40,8 +40,27 @@
                             </div>
                         </div>
                     </form>
+                    {{-- 削除ボタン --}}
+                    <form id="delete_{{ $image->id }}" method="post"
+                        action="{{ route('owner.images.destroy', ['image' => $image->id]) }}">
+                        @csrf
+                        @method('delete')
+                        <div class="p-2 w-full flex justify-around mt-32">
+                            <a href="#" data-id="{{ $image->id }}"
+                                onclick="deletePost(this)"
+                                class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded">削除する</a>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+    <script>
+        function deletePost(e) {
+            'use strict';
+            if (confirm('本当に削除してもいいですか?')) {
+                document.getElementById('delete_' + e.dataset.id).submit();
+            }
+        }
+    </script>
 </x-app-layout>
