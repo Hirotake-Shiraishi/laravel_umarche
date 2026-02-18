a4ef866c174b686b086e27f40d1bcbd45069232d
hirotake-shiraishi <srishrtk710@gmail.com>
2026-02-19 05:06:03 +0900

    Stockテーブルの作成

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
 use App\Models\secondaryCategory;
 use App\Models\Image;
+use App\Models\Stock;
 
 class Product extends Model
 {
@@ -26,5 +27,11 @@ class Product extends Model
     public function imageFirst()
     {
         return $this->belongsTo(Image::class, 'image1');
+    }
+
+    // 一対多のリレーション　親モデル -> 子モデル
+    public function stock()
+    {
+        return $this->hasMany(Stock::class);
     }
 }
