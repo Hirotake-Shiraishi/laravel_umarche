a68b8d2208f572ebb8d9c283c9f3c3ea6b3a20b7
hirotake-shiraishi <srishrtk710@gmail.com>
2026-02-18 13:06:49 +0900

    Product Index Eager Loading

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -4,80 +4,80 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Product;
+use App\Models\Owner;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function __construct()
     {
-        //
+        $this->middleware('auth:owners');
+
+        // クロージャを使ったコントローラミドルウェア
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('product'); //URLパラメータ productのid取得
+
+            if (!is_null($id)) {
+
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+
+                // セッションのログインIDと相違があったら
+                if ($productsOwnerId !== Auth::id()) {
+                    abort(404); // 404画面表示
+                }
+            }
+            return $next($request);
+        });
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    public function index()
+    {
+        // $products = Owner::findOrFail(Auth::id())->shop->product;
+
+        // リレーション関連データを事前読み込みした　ログインオーナーのOwnerインスタンス
+        $owner = Owner::with('shop.product.imageFirst')
+            ->findOrFail(Auth::id());
+
+        // ログインオーナーの商品一覧
+        $products = $owner->shop->product;
+
+        return view('owner.products.index', compact('products'));
+    }
+
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
