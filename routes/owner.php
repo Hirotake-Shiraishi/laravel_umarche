275a436bf4ac69a1c62488f16f70c95148d334a6
hirotake-shiraishi <srishrtk710@gmail.com>
2026-02-18 09:56:23 +0900

    Product 初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -10,18 +10,9 @@ use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
 use App\Http\Controllers\Owner\ImageController;
+use App\Http\Controllers\Owner\ProductController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('owner.welcome');
@@ -40,6 +31,11 @@ Route::prefix('shops')
 
 //image リソースコントローラへのルート
 Route::resource('images', ImageController::class)
+    ->middleware('auth:owners')
+    ->except(['show']);
+
+// product リソースコントローラへのルート
+Route::resource('products', ProductController::class)
     ->middleware('auth:owners')
     ->except(['show']);
 
