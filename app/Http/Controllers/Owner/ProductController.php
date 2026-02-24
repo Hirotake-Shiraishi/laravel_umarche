7ce1cab89cf477012b86ce7986dc23a6a64de335
hirotake-shiraishi <srishrtk710@gmail.com>
2026-02-24 13:43:12 +0900

    Product Destroy

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -212,6 +212,10 @@ class ProductController extends Controller
 
     public function destroy($id)
     {
-        //
+        // DB Imagesテーブルのレコードを削除
+        product::findOrFail($id)->delete();
+
+        return redirect()->route('owner.products.index')
+            ->with(['message' => '商品を削除しました。', 'status' => 'alert']);
     }
 }
