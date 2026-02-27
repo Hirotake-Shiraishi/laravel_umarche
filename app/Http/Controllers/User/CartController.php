7419049ba9a4ad351d48e7f830c2acf08e062c24
hirotake-shiraishi <srishrtk710@gmail.com>
2026-02-27 22:37:09 +0900

    Cart cart.blade

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -23,9 +23,9 @@ class CartController extends Controller
             $totalPrice += $product->price * $product->pivot->quantity;
         }
 
-        dd($products, $totalPrice);
+        // dd($products, $totalPrice);
 
-        return view('user.cart.index', compact('totalPrice'));
+        return view('user.cart', compact('products', 'totalPrice'));
     }
 
     public function add(Request $request)
