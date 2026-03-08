5bd9944f4ca35f3573eea99752b28b14532e5771
hirotake-shiraishi <srishrtk710@gmail.com>
2026-03-09 07:49:54 +0900

    キュー・ジョブ・ワーカー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,6 +10,7 @@ use App\Models\Stock;
 use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -41,8 +42,12 @@ class ItemController extends Controller
     {
         // dd($request);
 
-        Mail::to('test@example.com') //受信者の指定
-            ->send(new TestMail()); //Mailableクラス
+        // 同期的にメールを送信する場合
+        // Mail::to('test@example.com') //受信者の指定
+        //     ->send(new TestMail()); //Mailableクラス
+
+        // 非同期でメールを送信する場合
+        SendThanksMail::dispatch();
 
         $categories = PrimaryCategory::with('secondary')
             ->get();
