98ff965c4aef114044b13ae80e18d17a43a4b310
hirotake-shiraishi <srishrtk710@gmail.com>
2026-03-11 17:58:44 +0900

    ユーザー向け商品購入メールの作成

--- app/Jobs/SendThanksMail.php
+++ app/Jobs/SendThanksMail.php
@@ -9,30 +9,32 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\TestMail;
+use App\Mail\ThanksMail;
 
 class SendThanksMail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    // ジョブクラスで、事前にパブリックプロパティを定義し、
+    // コンストラクタの中で設定することで、コントローラから渡ってくる値を受け取ることができる。
+    public $products;
+    public $user;
+
+    public function __construct($products, $user)
     {
-        //
+        $this->products = $products;
+        $this->user = $user;
     }
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
+
     public function handle()
     {
-        Mail::to('test@example.com') //受信者の指定
-            ->send(new TestMail()); //Mailableクラス
+        // Mail::to('test@example.com') //メールの送信先の指定
+        //     ->send(new TestMail()); //Mailableクラス
+
+
+        // メールの設定
+        Mail::to($this->user) //メールの送信先の指定
+            ->send(new ThanksMail($this->products, $this->user)); //コントローラから渡ってきた情報を続いて、Mailableクラスに渡す
     }
 }
