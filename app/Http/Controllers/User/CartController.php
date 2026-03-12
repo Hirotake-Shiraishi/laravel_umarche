4712b1965a684bddca4aa76751b35df43e17f3ac
hirotake-shiraishi <srishrtk710@gmail.com>
2026-03-12 12:44:31 +0900

    オーナー向け商品販売メールの作成

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -10,6 +10,7 @@ use App\Models\User;
 use App\Models\Stock;
 use App\Services\CartService;
 use App\Jobs\SendThanksMail;
+use App\Jobs\SendOrderedMail;
 
 class CartController extends Controller
 {
@@ -71,20 +72,6 @@ class CartController extends Controller
 
     public function checkout()
     {
-        ////
-        $items = Cart::where('user_id', Auth::id())->get(); //where句は最後にget()が必要
-
-        $products = CartService::getItemsInCart($items);
-
-        $user = User::findOrfail(Auth::id());
-
-        // 非同期でメールを送信
-        // jobクラスに、引数として「商品情報」と「ユーザー情報」を渡す
-        SendThanksMail::dispatch($products, $user);
-
-        dd('ユーザーサンクスメール送信テスト');
-        ////
-
         $user = User::findOrfail(Auth::id());
         $products = $user->products;
 
@@ -149,6 +136,30 @@ class CartController extends Controller
 
     public function success()
     {
+        ////
+        $items = Cart::where('user_id', Auth::id())->get(); //where句は最後にget()が必要
+
+        $products = CartService::getItemsInCart($items);
+
+        $user = User::findOrfail(Auth::id());
+
+        // 非同期でメールを送信
+        // jobクラスに、引数として「商品情報」と「ユーザー情報」を渡す
+
+        // ユーザーに感謝メールを送信
+        SendThanksMail::dispatch($products, $user);
+
+        // オーナーに注文メールを送信
+        // 商品により、オーナーが異なるため、foreachでループ処理する。
+        // Mailtrap等のレート制限（1秒あたりのメール数）を避けるため、各ジョブに遅延を付与
+        foreach($products as $index => $product){ // インデックス番号も取得
+            SendOrderedMail::dispatch($product, $user)
+                ->delay(now()->addSeconds(5 + $index * 5));
+        }
+
+        // dd('ユーザーサンクスメール送信テスト');
+        ////
+
         // カート内の商品を削除
         Cart::where('user_id', Auth::id())->delete();
 
