535b29bd42faf3afeec08c32f66d9b45a3d70c37
hirotake-shiraishi <srishrtk710@gmail.com>
2026-04-13 18:40:00 +0900

    ngrok使用時 https接続を強制する処理の追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        // ngrokの場合はHTTPSに強制
+        if (str_contains(request()->getHost(), 'ngrok')) {
+            URL::forceScheme('https');
+        }
+
         // ownerから始まるURLの場合、セッションクッキー名を変更
         if (request()->is('owner*')) {
             config(['session.cookie' => config('session.cookie_owner')]);
