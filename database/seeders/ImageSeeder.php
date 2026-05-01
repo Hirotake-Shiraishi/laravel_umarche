ffed2f77455d56e4d215a4d0dfa71f698259e787
hirotake-shiraishi <srishrtk710@gmail.com>
2026-05-02 06:08:22 +0900

    chore: シーダー・ファクトリーのデータ追加

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -12,32 +12,122 @@ class ImageSeeder extends Seeder
         DB::table('images')->insert([
             [
                 'owner_id' => 1,
-                'filename' => 'sample1.jpg',
+                'filename' => 'sample01.jpg',
                 'title' => null
             ],
             [
                 'owner_id' => 1,
-                'filename' => 'sample2.jpg',
+                'filename' => 'sample02.jpg',
                 'title' => null
             ],
             [
                 'owner_id' => 1,
-                'filename' => 'sample3.jpg',
+                'filename' => 'sample03.jpg',
                 'title' => null
             ],
             [
                 'owner_id' => 1,
-                'filename' => 'sample4.jpg',
+                'filename' => 'sample04.jpg',
                 'title' => null
             ],
             [
-                'owner_id' => 1,
-                'filename' => 'sample5.jpg',
+                'owner_id' => 2,
+                'filename' => 'sample05.jpg',
                 'title' => null
             ],
             [
-                'owner_id' => 1,
-                'filename' => 'sample6.jpg',
+                'owner_id' => 2,
+                'filename' => 'sample06.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 2,
+                'filename' => 'sample07.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 2,
+                'filename' => 'sample08.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 3,
+                'filename' => 'sample09.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 3,
+                'filename' => 'sample10.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 3,
+                'filename' => 'sample11.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 3,
+                'filename' => 'sample12.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 4,
+                'filename' => 'sample13.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 4,
+                'filename' => 'sample14.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 4,
+                'filename' => 'sample15.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 4,
+                'filename' => 'sample16.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 5,
+                'filename' => 'sample17.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 5,
+                'filename' => 'sample18.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 5,
+                'filename' => 'sample19.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 5,
+                'filename' => 'sample20.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 6,
+                'filename' => 'sample21.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 6,
+                'filename' => 'sample22.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 6,
+                'filename' => 'sample23.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 6,
+                'filename' => 'sample24.jpg',
                 'title' => null
             ],
         ]);
